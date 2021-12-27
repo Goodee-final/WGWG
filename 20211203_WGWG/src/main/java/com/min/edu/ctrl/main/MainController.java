7bf998ae6db6ac23f80c0419a1d0ca239c5831f1
sunne090 <seonhui7622@gmail.com>
2021-12-28 04:07:02 +0900

    로그인 회원 정보 가져오기 수정

--- 20211203_WGWG/src/main/java/com/min/edu/ctrl/main/MainController.java
+++ 20211203_WGWG/src/main/java/com/min/edu/ctrl/main/MainController.java
@@ -9,13 +9,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.min.edu.model.approval.IApprovalService;
 import com.min.edu.model.emp.IEmpService;
 import com.min.edu.model.main.IMainService;
+import com.min.edu.model.worklog.IWorkLogService;
 import com.min.edu.vo.emp.Emp;
 import com.min.edu.vo.notice.NoticeVO;
 
@@ -31,7 +30,7 @@ public class MainController {
 	private IMainService mainService;
 	
 	@Autowired
-	private IApprovalService appService;
+	   private IWorkLogService workLogService;
 	
 	@RequestMapping(value="/home.do", method=RequestMethod.GET)
 	public String home(Model model, HttpSession session) {
@@ -40,7 +39,7 @@ public class MainController {
 		model.addAttribute("noticeList", noticeList);
 		logger.info("로그인한 사원번호{}",session.getAttribute("loginEmp"));
 	    int emp_no = (Integer)session.getAttribute("loginEmp");
-	    Emp emp = appService.selectEmpInfo(emp_no);
+	    Emp emp = workLogService.selectEmpNo(emp_no);
 	    model.addAttribute("emp",emp);
 //	    session.setAttribute("loc", "./main.do");
 		return "index";
@@ -54,7 +53,7 @@ public class MainController {
 		model.addAttribute("noticeList", noticeList);
 		logger.info("로그인한 사원번호{}",session.getAttribute("loginEmp"));
 	    int emp_no = (Integer)session.getAttribute("loginEmp");
-	    Emp emp = appService.selectEmpInfo(emp_no);
+	    Emp emp = workLogService.selectEmpNo(emp_no);
 	    model.addAttribute("emp",emp);
 	    
 	    session.setAttribute("loc", "./main.do");
